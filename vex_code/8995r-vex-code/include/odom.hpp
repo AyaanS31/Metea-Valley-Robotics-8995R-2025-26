853de92d5529790fb0586911d3de4458c39dc982
AyaanS31 <altafhussain.shaik@gmail.com>
2025-10-19 21:52:48 -0500

    drivetrain
    drivetrain code with correct ports

--- vex_code/8995r-vex-code/include/odom.hpp
+++ vex_code/8995r-vex-code/include/odom.hpp
@@ -9,7 +9,8 @@
 const double track_width = 10.5; // in inches 
 const double strafe_center_offset = 5.75; // in inches
 const double vertical_offset = 0.85; // in inches
-const double pod_tick_to_inch = (3.25*M_PI)/360.0; // wheel diameter 3.25 inches, 360 ticks per revolution
+const double pod_tick_to_inch = (2.0*M_PI)/360.0; // wheel diameter 2.0 inches, 360 ticks per revolution
+const double motor_degree_to_inch = (3.25*M_PI)/360.0; // wheel diameter 3.25 inches, 360 degrees per revolution
 
 // Odometry task function
 void odom_task(void* param);
